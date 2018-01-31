beea4a20813959576300a2b9395088b4c770dcaa
Kolya Venturi <kolya.venturi@enx3s.com>
2018-01-31 09:31:29 -0700

    Added iteration 2 tests

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -11,7 +11,15 @@ class TranslatorTest < Minitest::Test
 
   def test_translator_does_eng_to_morse
     translator = Translator.new
-    assert_equal "......-...-..--- .-----.-..-..-..", translator.eng_to_morse("hello world") 
+    assert_equal "......-...-..--- .-----.-..-..-..", translator.eng_to_morse("hello world")
+
+    # Case insensitive
+    assert_equal "......-...-..--- .-----.-..-..-..", translator.eng_to_morse("hELlO wORlD")
+
+    # Numbers
+    assert_equal "-......-.. .-.-.. ...-- ..........--....", translator.eng_to_morse("There are 3 ships")
   end
+
+
 
 end
