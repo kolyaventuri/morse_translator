f5792cbc1d62fe41fb93f2597cbf8fb59e7cb476
Kolya Venturi <kolya.venturi@enx3s.com>
2018-01-31 09:26:35 -0700

    Added iteration 1 tests, eng_to_morse

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -1,1 +1,17 @@
-require_relative 'test_helper'
+require_relative 'test_helper.rb'
+
+require './lib/Translator.rb'
+
+class TranslatorTest < Minitest::Test
+
+  def test_translator_object_exists
+    translator = Translator.new
+    assert_instance_of Translator, translator
+  end
+
+  def test_translator_does_eng_to_morse
+    translator = Translator.new
+    assert_equal "......-...-..--- .-----.-..-..-..", translator.eng_to_morse("hello world") 
+  end
+
+end
